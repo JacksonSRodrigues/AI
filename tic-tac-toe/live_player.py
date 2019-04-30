82d41b0ea6459e75525aaf6e90195ee975767fcb
Jackson Sunny Rodrigues <jackson.sunny.rodrigues@gmail.com>
2019-04-30 22:47:14 +0530

    Adding a player to give input through commandline

---
+++ tic-tac-toe/live_player.py
@@ -0,0 +1,20 @@
+from board import Player
+
+class LivePlayer(Player):
+
+    def __init__(self, name, avatar):
+        super().__init__(name,avatar)
+
+    def choose_move(self,valid_moves):
+        print(valid_moves)
+        recieved_valid_move = False
+        move = None
+        while not recieved_valid_move:
+            user_input = input('Enter your Move from Above : ')
+            move = tuple(map(int, user_input.split(',')))
+            if move in valid_moves:
+                recieved_valid_move = True
+            else:
+                print('Invalid Move!')
+        return move
+
--- tic-tac-toe/play.py
+++ tic-tac-toe/play.py
@@ -1,36 +1,50 @@
 from tic_tac_toe import TicTacToe, Player, Status, Result
+from live_player import LivePlayer
 
 board = TicTacToe()
-board.add_new_player(Player('Jack', 'x'))
+board.add_new_player(LivePlayer('Jack', 'x'))
 board.add_new_player(Player('Jill','o'))
 
 def play_game(board):
     while board.status is not Status.Complete:
-        print('----  {}'.format(board.status))
-        print(board.available_moves())
+        print('-------------')
         p = board.next_player()
+        print('Move for {}'.format(p.avatar))
         available_moves = board.available_moves()
         move = p.choose_move(available_moves) 
-        print('Move made by {}'.format(p.avatar))
         board.make_move(p,move)
         board.visualize_state()
 
-draws = 0
-player1 = 0
-player2 = 0
-
-
-for i in range(100):
-    print('Game {}'.format(i))
-    board.reset()
+def test_live_player():
+    board = TicTacToe()
+    board.add_new_player(LivePlayer('Jack', 'x'))
+    board.add_new_player(Player('Jill','o'))
     play_game(board)
-    if board.result == Result.Draw:
-        draws += 1
-    else:
-        if board.players.index(board.winner) == 0:
-            player1 += 1
+
+test_live_player()
+
+def test_board_statistics(p1,p2,test_count=100):
+    draws = 0
+    player1 = 0
+    player2 = 0
+
+    board = TicTacToe()
+    board.add_new_player(p1)
+    board.add_new_player(p2)
+
+    for i in range(test_count):
+        print('Game {}'.format(i))
+        board.reset()
+        play_game(board)
+        if board.result == Result.Draw:
+            draws += 1
         else:
-            player2 += 1
+            if board.players.index(board.winner) == 0:
+                player1 += 1
+            else:
+                player2 += 1
+
+    print('Draws: {}   Player 1 Won: {}     Player 2 Won: {}'.format(draws,player1,player2))
 
 
-print('Draws: {}   Player 1 Won: {}     Player 2 Won: {}'.format(draws,player1,player2))
+#test_board_statistics(Player('Jack', 'x'),Player('Jill','o'))
