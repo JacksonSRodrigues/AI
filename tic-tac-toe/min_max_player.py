6c0453ff4752bf3ffdf592f62d2d93305f069a52
Jackson Sunny Rodrigues <jackson.sunny.rodrigues@gmail.com>
2019-05-05 19:34:52 +0530

    Add Comments to MinMax Player

--- tic-tac-toe/min_max_player.py
+++ tic-tac-toe/min_max_player.py
@@ -15,6 +15,10 @@ class MinMaxPlayer(Player):
         
 
     def min(self,board):
+        """
+        Minimize the Players Move, i.e this is to calculate the impact of opposite players counter move.
+        So here we find the minimum of the possible values that the parent move can lead to. Lowest value being Loosing the game.
+        """
         hash_value = board.hash_value()
         if hash_value in self.cache:
             return self.cache[hash_value]
@@ -22,26 +26,33 @@ class MinMaxPlayer(Player):
         score = DRAW
         action = None
 
-        # For handling the cases win happende on last move
+        # Check for the end of Game.
         if board.winner is not None:
             if self == board.winner:
                 score = WIN
             else:
                 score = LOSE
-        else:
-            for next_move in board.available_moves():
-                eval_board = self.board_copy(board)
-                eval_board.make_move(self._competing_player(eval_board),next_move) 
-                next_score, _ = self.max(eval_board)
-                if next_score < score or action is None:
+        else: # If Game is not finished by the last move, Lets check the possibilities, of the remaining moves.
+            for next_move in board.available_moves(): 
+                eval_board = self.board_copy(board) # Create a new copy of the board.
+                eval_board.make_move(self._competing_player(eval_board),next_move) # Make the move, by Oppenent.
+                next_score, _ = self.max(eval_board) # Calculate the result which can be achieved by our Counter move.
+                if next_score < score or action is None: # Save the minimal Score yet, and the move.
                     score = next_score
                     action = next_move
-                    self.cache[hash_value] = (next_score,next_move)
-                if score == LOSE:
+                    self.cache[hash_value] = (next_score,next_move) # Cache for performance.
+                if score == LOSE: # Stop iteration if the Opponents move leads us to faliure.
                     break
         return score, action
 
+
+
     def max(self, board:TicTacToe):
+        """
+        MMaximize the Players Move, i.e this is to calculate the maximum impact a player can male.
+        So here we find the Maximum of the possible result that the parent move can lead to, even an opponent makes the move.
+        """
+
         hash_value = board.hash_value()
         if hash_value in self.cache:
             return self.cache[hash_value]
@@ -49,31 +60,38 @@ class MinMaxPlayer(Player):
         score = DRAW
         action = None
 
-        # For handling the cases win happende on last move
+        # Check for the end of Game.
         if board.winner is not None:
             if self == board.winner:
                 score = WIN
             else:
                 score = LOSE
-        else:
+        else: # If Game is not finished by the last move, Lets check the possibilities, of the remaining moves.
             for next_move in board.available_moves():
-                eval_board = self.board_copy(board)
-                eval_board.make_move(self,next_move)
-                next_score, _ = self.min(eval_board)
+                eval_board = self.board_copy(board) # Create a new copy of the board.
+                eval_board.make_move(self,next_move) # Make a move as Self.
+                next_score, _ = self.min(eval_board) # Calculate the score that can be lead by Opponents move.
                 if next_score > score or action is None:
                     score = next_score
                     action = next_move
-                    self.cache[hash_value] = (next_score,next_move)
-                if score == WIN:
+                    self.cache[hash_value] = (next_score,next_move) # Cache for performance
+                if score == WIN: #Stop iteration if the move can lead to a WIN
                     break
         return score, action
 
+
     def choose_move(self,board: TicTacToe):
+        """
+        Choose a Move for Player, which Maximizes our winning Chance.
+        """
         eval_board = self.board_copy(board)
         _, move = self.max(eval_board)
         return move
 
     def board_copy(self, board):
+        """
+        Make a copy of the board.
+        """
         eval_board = TicTacToe(nodes=list(map(lambda row: list(row),board.nodes)))
         eval_board.players = list(board.players)
         eval_board.last_player = board.last_player
@@ -81,18 +99,10 @@ class MinMaxPlayer(Player):
         eval_board.status = board.status
         eval_board.winner = board.winner
         return eval_board
+        
 
     def _competing_player(self,board):
+        """
+        Find the opponent.
+        """
         return list(filter(lambda p: p != self, board.players))[0]
-
-    def did_win(self,board):
-        is_winner = False
-        if board.status == Status.Complete and board.winner == self:
-            is_winner = True
-        return is_winner
-
-    def did_lose(self,board):
-        is_loser = False
-        if board.status == Status.Complete and board.winner != self and board.status != Result.Draw:
-            is_loser = True
-        return is_loser
